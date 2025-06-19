d25e877cab15b4a454d14d95416b25c8d7b40655
Prince Chothani <princechothani53@gmail.com>
2025-06-19 16:32:59 +0530

    final commit

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 
-import spacy
-spacy.load('en_core_web_sm')
+import en_core_web_sm
+nlp = en_core_web_sm.load()
  # require for pyresparser
 
 import requests 
